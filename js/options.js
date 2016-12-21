4ec88fa16b022d6157b693c556192196cd78c842
halfopen <halfopen@yeah.net>
2016-12-21 17:29:18 +0800

    样式修改

--- js/options.js
+++ js/options.js
@@ -1,18 +1,15 @@
 console.info("设置页面");
 
-
 function 加载配置() {
     //加载配置
     var 本地是否显示歌词 = browser.storage.local.get('是否显示歌词');
     本地是否显示歌词.then(function(res) {
         console.info("加载配置", res);
         var 歌词开关元素 = document.getElementById("歌词开关");
-        if (res.是否显示歌词) {
+        if (res && res.是否显示歌词) {
             歌词开关元素.checked = true;
-            //   browser.storage.local.set({ '是否显示歌词': false });
         } else {
             歌词开关元素.checked = false;
-            //    browser.storage.local.set({ '是否显示歌词': true });
         }
     }, function() {
         console.info("加载本地配置失败");
---
+++ js/popup.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    console.info("1");
+    var 歌手名元素 = $(".song-name");
+    歌手名元素.html(111);
+});
